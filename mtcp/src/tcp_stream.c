f9b192e68afd0aa6c4696b44e8fbe750ab212b0e
Flasew <af141_l@icloud.com>
2020-01-13 10:20:38 -0800

    init seq to 0 to make my life easier...

--- mtcp/src/tcp_stream.c
+++ mtcp/src/tcp_stream.c
@@ -307,8 +307,8 @@ CreateTCPStream(mtcp_manager_t mtcp, socket_map_t socket, int type,
 	stream->sndvar->nif_out = GetOutputInterface(stream->daddr, &is_external);
 	stream->is_external = is_external;
 
-	stream->sndvar->iss = rand_r(&next_seed) % TCP_MAX_SEQ;
-	//stream->sndvar->iss = 0;
+	// stream->sndvar->iss = rand_r(&next_seed) % TCP_MAX_SEQ;
+	stream->sndvar->iss = 0;
 	stream->rcvvar->irs = 0;
 
 	stream->snd_nxt = stream->sndvar->iss;
